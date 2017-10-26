8259356607f203cec4b52672e75d66b954476a76
kNelakonda <kavi.nelakonda@gmail.com>
2017-10-25 18:50:49 -0700

    final hallways without trigger(nithin is awesome)

---
+++ Halloween Test/Assets/OnLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnLight : MonoBehaviour {
+
+	public Light light;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter (Collider other){
+		light.enabled = !light.enabled;
+		Invoke ("LightOff", 2);
+	}
+
+	void LightOff(){
+		light.enabled = !light.enabled;
+	}
+}
