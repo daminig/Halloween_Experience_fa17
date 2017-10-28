facdabfd3d1341a51a3d328cf53799c494fe4aab
makarasaigon <makarasaigon@gmail.com>
2017-10-28 16:40:19 -0700

    wacky zappy commit #2: kavi messed up the coordinates

---
+++ Halloween Test/Assets/ElevatorDoorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoorScript : MonoBehaviour {
+
+        public Animation anim;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+        
+        void OnTriggerEnter(Collider other) {
+            anim.Play(); /*anim.clip.name*/
+        }
+}
