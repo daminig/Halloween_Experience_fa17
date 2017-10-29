daf2c5fa0be38ded7cf260d0f7d96adff9ffd09b
IkedaYui <mjyui16@airbears2-10-142-39-151.airbears2.1918.berkeley.edu>
2017-10-29 02:50:47 -0700

    Animator: Cube, Animation: t-junction, Trigger script: tjunc

---
+++ Halloween Test/Assets/Standard Assets/Utility/tjunc trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tjunctrigger : MonoBehaviour {
+	public GameObject square;
+	// Use this for initialization
+
+	void onTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player") {
+			square.GetComponent<Animation> ().Play ("t-junction");
+			}
+	}
+	// Update is called once per frame
+
+}
---
+++ Halloween Test/Assets/tjunc.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tjunc : MonoBehaviour {
+
+	public Animator anim;
+	public GameObject square;
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+	void Start() {
+		anim = square.GetComponent<Animator>();
+	}
+	void OnTriggerEnter (Collider other) {
+
+		//if (other.gameObject.tag == "Player") {
+		//	square.GetComponent<Animator>().Play ("Cube");
+		//}
+		Debug.Log("hi");
+		if (other.gameObject.tag == "Player") {
+			anim.SetBool ("TriggerBool", true);
+		}
+	}
+	// Update is called once per frame
+
+}
