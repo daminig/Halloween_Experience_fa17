3842e1b8b70e4e58786002dd6b05992f2249227b
makarasaigon <makarasaigon@gmail.com>
2017-10-28 21:52:28 -0700

    1

--- Halloween Test/Assets/ElevatorDoorScript.cs
+++ Halloween Test/Assets/ElevatorDoorScript.cs
@@ -6,17 +6,18 @@ public class ElevatorDoorScript : MonoBehaviour {
 
         public Animation anim;
 
-
 	// Use this for initialization
 	void Start () {
+            
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-        
+        /*
         void OnTriggerEnter(Collider other) {
-            anim.Play(); /*anim.clip.name*/
+            anim.Play(); /*anim.clip.name
         }
+        */
 }
